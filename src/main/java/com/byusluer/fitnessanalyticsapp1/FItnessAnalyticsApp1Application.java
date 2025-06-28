e765755cc075f0bb737f3a235288cb81bbd285eb
usluerL <byusluerdev@gmail.com>
2025-06-28 21:20:32 +0300

    first commit, initial setup

---
+++ src/main/java/com/byusluer/fitnessanalyticsapp1/FItnessAnalyticsApp1Application.java
@@ -0,0 +1,13 @@
+package com.byusluer.fitnessanalyticsapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FItnessAnalyticsApp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FItnessAnalyticsApp1Application.class, args);
+    }
+
+}
